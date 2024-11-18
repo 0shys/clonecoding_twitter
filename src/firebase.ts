05fd664cb0105a0625a0ca39b47c7517a7ff052b
0shys <solra031@hknu.ac.kr>
2024-11-18 18:27:50 +0900

    loading + firebase

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhi8bLGU13oKy7kMhNepy93aCRgkgRrjo",
+  authDomain: "twitter-reloaded-86f30.firebaseapp.com",
+  projectId: "twitter-reloaded-86f30",
+  storageBucket: "twitter-reloaded-86f30.firebasestorage.app",
+  messagingSenderId: "196522428950",
+  appId: "1:196522428950:web:6ab75c44c83cf7a6e98337"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
