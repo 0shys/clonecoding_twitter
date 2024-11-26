69eb0a4fd9ed42fa1b00cd230420bd750437af23
0shys <solra031@hknu.ac.kr>
2024-11-27 04:48:23 +0900

    add layout, add post-tweet-form

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,13 @@
+import PostTweetForm from "../components/post-tweet-form";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+
 export default function Home() {
-    return <h1>Home!</h1>
+    return (
+        <Wrapper>
+            <PostTweetForm />
+        </Wrapper>
+    )
 }
